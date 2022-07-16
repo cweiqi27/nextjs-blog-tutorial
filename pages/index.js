eb67287b8a2211ff050a255bd8bf1c437d50ed27
cweiqi27 <97137887+cweiqi27@users.noreply.github.com>
2022-07-16 11:24:55 +0800

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read {' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -18,9 +22,9 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="posts/first-post" className="card">
+            <h3>First Post &rarr;</h3>
+            <p>Ayo here's my first post</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
